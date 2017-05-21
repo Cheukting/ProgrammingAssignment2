5a822d49299ea92a714c471609ee1e1edb0266c6
Cheukting <cheukting.ho@gmail.com>
2017-05-21 12:22:38 +0100

    Summit HW2

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,17 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## With this set of functions, one can calculate the inverse of an invertable matrix
+## and store it in cache by creating a special "matrix" object using makeCacheMatrix 
+## and calling cacheSolve.
+
+## ===============================
+
+##  makeCacheMatrix creates a special "matrix" object,
+##  which is really a list containing 4 functions to
+
+##  1.set the value of the matrix
+##  2.get the value of the matrix
+##  3.set the value of the inverse of the matrix
+##  4.get the value of the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- matrix()
@@ -18,10 +28,15 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+##  cacheSolve calculates the inverse of the matrix in 
+##  the "matrix" object created with makeCacheMatrix. 
+##  However, it first checks to see if the inverse has already been calculated. 
+##  If so, it gets the inverse from the cache and skips the computation.
+##  Otherwise, it calculates the inverse of the matrix
+##  and sets the value of the inverse in the cache via the setinv function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
   m <- x$getinv()
   if(!is.na(m[[1,1]])) {
     message("getting cached data")
